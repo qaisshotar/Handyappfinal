2641f6fa585b8ba07450648dbe7832146bd55f5c
qaisshotar <65866193+qaisshotar@users.noreply.github.com>
2021-04-27 09:56:39 +0300

    commit

---
+++ app/src/main/java/com/example/handymanfinal/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+ package com.example.handymanfinal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+ public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+delaysplaSplashScreen();
+
+    }
+
+     private void delaysplaSplashScreen() {
+
+        // Completable.timer(5, TimeUnit.SECONDS,
+              //   AndroidSchedulers.mainThread());
+
+                 }
+
+
+ }
